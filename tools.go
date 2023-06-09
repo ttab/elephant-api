c6a6b785ee663264757eae49e5cb90e303c8ff7b
Hugo Wetterberg <hugo@wetterberg.nu>
2023-06-09 09:35:24 +0200

    separate newsdoc protobuf declarations

--- tools.go
+++ tools.go
@@ -5,6 +5,7 @@ package tools
 
 import (
 	_ "github.com/navigacontentlab/twopdocs"
+	_ "github.com/ttab/newsdoc/cmd/newsdoc"
 	_ "github.com/twitchtv/twirp/protoc-gen-twirp"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
